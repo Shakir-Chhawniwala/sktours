a3488b2615982a42a816c4aa3274a118e355bffe
Shakir Chhawniwala <shakir@themetromaxgroup.com>
2023-12-27 22:53:42 +0530

    added more code

---
+++ controllers/tourController.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+const getALLTours = (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+
+
+}
