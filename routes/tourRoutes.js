203feefe2c8187b3e52c5c555c246719b7c75904
Shakir-Chhawniwala <64503320+Shakir-Chhawniwala@users.noreply.github.com>
2023-12-28 01:18:09 +0530

    changed file structure

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,19 @@
-const express = require('express')
+const express = require('express');
+const tourController = require('./../controllers/tourController');
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/").get().post()
+router.param('id', tourController.checkID);
 
-router.route("/:id").get().patch().delete()
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const userController = require('./../controllers/userController');
 
 const router = express.Router();
 
-router.route('/').get().post();
-router.route('/:id').get().patch().delete();
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
